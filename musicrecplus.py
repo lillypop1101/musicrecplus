baabc7103468d5fc96b7032c6b89e6c8c4fe5774
Aciel <122584962+lillypop1101@users.noreply.github.com>
2024-11-26 22:13:52 -0500

    Add files via upload

---
+++ musicrecplus.py
@@ -0,0 +1,29 @@
+"""
+Nov. 25, 2024
+Jem Riche, Naima Sana, Felicity Tabia
+We pledge our honor that we have abided by the Stevens Honor System.
+"""
+data = {}
+name = ""
+
+with open("musicrecplus.txt", "w+") as file:
+    name = input("Enter your name ( put a $ symbol after your name if you wish your preferences to remain private ): ")
+    if name not in data:
+        data[name] = []
+        # enter preferences before showing menu
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
