6730ac51f537227389cb8af41bab064db8c31323
Felicity Tabia <felicitygerielle@gmail.com>
2024-11-27 17:10:22 -0500

    Update musicrecplus.py

--- musicrecplus.py
+++ musicrecplus.py
@@ -12,18 +12,26 @@ with open("musicrecplus.txt", "w+") as file:
         data[name] = []
         # enter preferences before showing menu
 
+def save():
+    " When the user selects 'q', it saves and quits. """
+    pass
 
-    
-    
-
-
-
-
-
-
-
-
-
-
-
+def print_menu():
+    """ Gives the user a menu of options to input. """
+    valid_options = ['e', 'r', 'p', 'h', 'm', 'q']
+    while True:
+        option = input("""
+Enter a letter to choose an option:
+e - Enter preferences
+r - Get recommendations
+p - Show most popular artists
+h - How popular is the most popular
+m - Which user has the most likes
+q - Save and quit
+""")
+        x = option.strip()
+        if x in valid_options:
+            return x
+        else:
+            print("Invalid option chosen. Please try again.")
     
